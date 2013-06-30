bc40350c443960c8956fb76530cbe2114e7df863
Albert Cervera i Areny <albert@nan-tic.com>
2013-06-30 17:10:41 +0200

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+from trytond.pool import Pool
+from .forecast import *
+
+
+def register():
+    Pool.register(
+        ForecastReport,
+        module='account_payment_forecast', type_='report')
