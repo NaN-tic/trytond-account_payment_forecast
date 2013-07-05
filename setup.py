9784c08ccf7be304d0000659792d0064db9ef689
resteve <resteve@zikzakmedia.com>
2013-07-05 13:08:30 +0200

    Setup: description

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ tests_require = ['proteus >= %s.%s, < %s.%s' %
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='',
+    description='Tryton account payment forecast module',
     long_description=read('README'),
     author='NaN·tic',
     url='http://www.nan-tic.com/',
