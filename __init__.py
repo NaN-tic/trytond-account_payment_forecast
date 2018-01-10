da67ce23b936c57c4a97a7a8268cdd20826f5d63
C?dric Krier <ced@b2ck.com>
2018-01-10 16:33:52 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-#This file is part account_payment_forecast module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part account_payment_forecast module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
-from .forecast import *
+from . import forecast
 
 
 def register():
     Pool.register(
-        ForecastReport,
+        forecast.ForecastReport,
         module='account_payment_forecast', type_='report')
